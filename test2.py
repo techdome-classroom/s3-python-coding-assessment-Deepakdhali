48bcd3c079ca6c090a997de702ccee4194b3d1c9
Deepakdhali <141645834+Deepakdhali@users.noreply.github.com>
2024-10-08 15:38:38 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,32 +1,22 @@
-import unittest
-from program2 import Solution
+def romanToInt(s: str) -> int:
+    
+    roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    
+   
+    total = 0
+    
+   
+    for i in range(len(s)):
+       
+        if i < len(s) - 1 and roman_map[s[i]] < roman_map[s[i + 1]]:
+            total -= roman_map[s[i]]
+        else:
+          
+            total += roman_map[s[i]]
+    
+    return total
 
-class TestRomanToInt(unittest.TestCase):
-    def setUp(self):
-        self.solution = Solution()
-
-    def test_example1(self):
-        self.assertEqual(self.solution.romanToInt("III"), 3)
-
-    def test_example2(self):
-        self.assertEqual(self.solution.romanToInt("LVIII"), 58)
-
-    def test_example3(self):
-        self.assertEqual(self.solution.romanToInt("MCMXCIV"), 1994)
-
-    def test_single_roman_digit(self):
-        self.assertEqual(self.solution.romanToInt("X"), 10)
-
-    def test_subtraction_rule(self):
-        self.assertEqual(self.solution.romanToInt("IV"), 4)
-        self.assertEqual(self.solution.romanToInt("IX"), 9)
-
-    def test_large_number(self):
-        self.assertEqual(self.solution.romanToInt("MMMCMXCIX"), 3999)
-
-    def test_empty_string(self):
-        self.assertEqual(self.solution.romanToInt(""), 0)
-
-if __name__ == '__main__':
-    unittest.main(argv=['first-arg-is-ignored'], exit=False)
-
+# Test cases
+print(romanToInt("III"))     
+print(romanToInt("LVIII"))   
+print(romanToInt("MCMXCIV")) 
